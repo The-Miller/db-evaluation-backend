b8164ec31c8f4fd6cb761b5ca301c03853d83a05
The-Miller <niang08boubacar@gmail.com>
2025-03-20 10:20:05 +0000

    Utilisation de process.env.PORT pour Render

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 const routes = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
+
 
 // Middleware
 app.use(cors());
